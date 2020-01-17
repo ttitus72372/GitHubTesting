ddafcb626450bcfeb434124ec7fd4e0627004061
ttitus72372 <tnt2594@gmail.com>
2020-01-17 16:14:40 +0000

    added some simple input variables

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ while x <= 10:
   x +=1
   print(x)
 #print(x)
-'''
+
 
 list1 = [1,4,7,9,11,34,65]
 for item in list1:
   #print(item)
   if item <= 20:
     print(item)
+
+'''
+numb1 = int(input("Please enter a number:\n"))
+numb2 = int(input("Please enter another number:\n"))
+numb3 = numb1 + numb2
+print(numb3)
